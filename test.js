3394446894088d5ae6b8be115b2943104efbf542
Tom Hummel <tphummel@gmail.com>
2015-01-29 18:53:04 -0800

    jshint

--- test.js
+++ test.js
@@ -1,32 +1,32 @@
 var test = require('tape'),
-    http = require('http');
+    http = require('http')
 
 test('health-server', function(t){
-  var server  = require('./');
+  var server  = require('./')
 
   http.get('http://localhost:3000/health', function(res){
-    var payload = '';
+    var payload = ''
 
-    t.equal(res.statusCode, 200);
+    t.equal(res.statusCode, 200)
 
     res.on('data', function(d){
-      payload += d;
+      payload += d
 
     }).on('end', function(){
-      health = JSON.parse(payload.toString());
+      var health = JSON.parse(payload.toString())
 
-      t.ok(health.ts, 'has field "ts"');
-      t.ok(health.pid, 'has field "pid"');
-      t.ok(health.uptime >= 0, 'has field "uptime"');
-      t.ok(health.memory, 'has field "memory"');
-      t.equal(health.status, 'ok');
+      t.ok(health.ts, 'has field "ts"')
+      t.ok(health.pid, 'has field "pid"')
+      t.ok(health.uptime >= 0, 'has field "uptime"')
+      t.ok(health.memory, 'has field "memory"')
+      t.equal(health.status, 'ok')
 
-      server.close();
-      t.end();
-    });
+      server.close()
+      t.end()
+    })
 
   }).on('error', function(e){
-    server.close();
-    t.fail(e);
-  });
-});
+    server.close()
+    t.fail(e)
+  })
+})
